63a8c68bc1ef6304031544da50836fba1d2bc1f8
Object NSS <agafonov.egorushka@gmail.com>
2024-08-21 13:46:51 +0300

    add tg.disableVerticalSwipes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,14 @@ import useLocalBalance from './hooks/useLocalBalance';
 import useMessages from './hooks/useMessages';
 
 function App() {
+  const [isTelegram, setIsTelegram] = useState("user" in window.Telegram.WebApp.initDataUnsafe);
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+        // Отключаем вертикальные свайпы
+        tg.disableVerticalSwipes(true);
+    }
+  }, [window.Telegram, window.Telegram.WebApp])
   const [isLoading, setIsLoading] = useState(true);
   const [showOnboarding, setShowOnboarding] = useState(!localStorage.getItem('onboardingComplete'));
   const [currentPage, setCurrentPage] = useState('home');
