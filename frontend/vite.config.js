bcfa8f78baf123cc2b21ac2902deb7c57919d141
Object NSS <agafonov.egorushka@gmail.com>
2024-09-02 02:10:16 +0300

    add changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,15 +23,6 @@ export default defineConfig({
         })
       ]
     }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        inject({
-          Buffer: ['Buffer', 'Buffer'],
-        }),
-      ],
-    },
   },
   resolve: {
     alias: {
