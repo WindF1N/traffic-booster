d494b000cbdd7f88d6c81cade4cd3fcbb59fd408
Object NSS <agafonov.egorushka@gmail.com>
2024-09-02 02:09:08 +0300

    add changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -34,4 +34,10 @@ export default defineConfig({
       ],
     },
   },
+  resolve: {
+    alias: {
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6'
+    }
+  },
 })
