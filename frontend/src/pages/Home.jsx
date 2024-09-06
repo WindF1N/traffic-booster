32358ae63c0084591a7221a13f6124923dc50b74
Object NSS <agafonov.egorushka@gmail.com>
2024-09-06 22:36:49 +0300

    add changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -47,22 +47,13 @@ function Home() {
     setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0);
   }, []);
   const handleClick = (event) => {
-    if (navigator.vibrate) {
-      try {
-        navigator.vibrate(200); // Вибрируем 200 миллисекунд
-      } catch (e) {
-        alert(e);
-      }
-    } else {
-      alert(navigator)
-    }
     if (isTouchDevice) return; // Если устройство поддерживает касания, ничего не делаем
     // Добавление надписи +1 с небольшим случайным смещением
     const x = event.clientX + (Math.random() - 0.5) * 200; // Случайное смещение по x
     const y = event.clientY + (Math.random() - 0.5) * 200; // Случайное смещение по y
     const newPlusOne = { x, y, id: Date.now() };
     setPlusOnes(prevPlusOnes => [...prevPlusOnes, newPlusOne].slice(-10)); // Обрезаем массив до последних 10 элементов
-    
+
     // Увеличение локального баланса
     setLocalBalance(localBalance + Number(account?.character?.multiplier));
   };
@@ -73,120 +64,131 @@ function Home() {
     const y = event.touches[0].clientY + (Math.random() - 0.5) * 200; // Случайное смещение по y
     const newPlusOne = { x, y, id: Date.now() };
     setPlusOnes(prevPlusOnes => [...prevPlusOnes, newPlusOne].slice(-10)); // Обрезаем массив до последних 10 элементов
-    
+
     // Увеличение локального баланса
     setLocalBalance(localBalance + Number(account?.character?.multiplier));
   };
   const claimFarming = () => {
     fetch(apiUrl+'/farming/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
     })
     .then(response => response.json())
     .then(data => {
-      if ('error' in data) {
-        addMessage({
-          type: 'error',
-          text: data.error,
-          name: 'Ошибка:'
-        })
-      } else {
-        setAccount({...account, balance: data.new_balance, farming: data.new_farming });
-        addMessage({
-          type: 'success',
-          text: data.message,
-          name: 'Успех:'
-        })
-      }
+        if ('error' in data) {
+          addMessage({
+              type: 'error',
+              text: data.error,
+              name: 'Ошибка:'
+          })
+        } else {
+          setAccount({...account, balance: data.new_balance, farming: data.new_farming });
+          addMessage({
+              type: 'success',
+              text: data.message,
+              name: 'Успех:'
+          })
+        }
     })
     .catch(error => {
-      addMessage({
-        type: 'error',
-        text: error,
-        name: 'Ошибка:'
-      })
-      console.error('Error:', error)
+        addMessage({
+            type: 'error',
+            text: error,
+            name: 'Ошибка:'
+        })
+        console.error('Error:', error)
     });
-  }
+}
   return (
     <>
-    <div className="relative flex flex-col h-screen overflow-hidden">
-    <img
-    className="absolute z-[-1] opacity-[0.1] rotate-[-30deg] scale-[2.49] inset-0 m-auto blur-lg"
-    src={bgImage}
-    alt=""
-    />
-    <div className="flex gap-[10px] px-[20px] mt-[5.07%]">
-    <div className="p-[10px] bg-[rgba(117,117,117,0.1)] rounded-[10px] backdrop-blur-[40px]">
-    <div className="text-[14px] leading-[18.06px] text-[#F1F1F1] font-[400]">Уровень:</div>
-    {account?.character?.type == "standart" &&
-      <div className="text-[20px] leading-[25.8px] font-[600] text-[#319BFF] mt-[11px]">{account?.character?.name}</div>}
-      {account?.character?.type == "silver" &&
-        <div className="text-[20px] leading-[25.8px] font-[600] text-[#9CFF11] mt-[11px]">{account?.character?.name}</div>}
-        {account?.character?.type == "gold" &&
-          <div className="text-[20px] leading-[25.8px] font-[600] text-[#FF11DF] mt-[11px]">{account?.character?.name}</div>}
+      <div className="relative flex flex-col h-screen overflow-hidden">
+        <img
+          className="absolute z-[-1] opacity-[0.1] rotate-[-30deg] scale-[2.49] inset-0 m-auto blur-lg"
+          src={bgImage}
+          alt=""
+        />
+        <div className="flex gap-[10px] px-[20px] mt-[5.07%]">
+          <div className="p-[10px] bg-[rgba(117,117,117,0.1)] rounded-[10px] backdrop-blur-[40px]">
+            <div className="text-[14px] leading-[18.06px] text-[#F1F1F1] font-[400]">Уровень:</div>
+            {account?.character?.type == "standart" &&
+            <div className="text-[20px] leading-[25.8px] font-[600] text-[#319BFF] mt-[11px]">{account?.character?.name}</div>}
+            {account?.character?.type == "silver" &&
+            <div className="text-[20px] leading-[25.8px] font-[600] text-[#9CFF11] mt-[11px]">{account?.character?.name}</div>}
+            {account?.character?.type == "gold" &&
+            <div className="text-[20px] leading-[25.8px] font-[600] text-[#FF11DF] mt-[11px]">{account?.character?.name}</div>}
           </div>
           <div className="p-[10px] bg-[rgba(117,117,117,0.1)] rounded-[10px] w-[100%] backdrop-blur-[40px]">
-          <div className="text-[14px] leading-[18.06px] text-[#F1F1F1] font-[400]">Баланс:</div>
-          <div className="text-[28px] leading-[36.12px] font-[600] text-[#FFD900] mt-[6px] flex items-center gap-[5px]">
-          {account?.balance?.amount && (Number(account?.balance?.amount) + localBalance).toLocaleString('de-DE', { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
-          <img
-          className="flex w-[20px] h-[20px] mt-[-4px]"
-          src={raster3dIcon}
-          alt=""
-          />
+            <div className="text-[14px] leading-[18.06px] text-[#F1F1F1] font-[400]">Баланс:</div>
+            <div className="text-[28px] leading-[36.12px] font-[600] text-[#FFD900] mt-[6px] flex items-center gap-[5px]">
+              {account?.balance?.amount && (Number(account?.balance?.amount) + localBalance).toLocaleString('de-DE', { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
+              <img
+                className="flex w-[20px] h-[20px] mt-[-4px]"
+                src={raster3dIcon}
+                alt=""
+              />
+            </div>
           </div>
-          </div>
-          </div>
-          <div className="px-[20px] mt-[10px] z-[3]">
+        </div>
+        <div className="px-[20px] mt-[10px] z-[3]">
           <div className="p-[10px] bg-[rgba(117,117,117,0.1)] rounded-[10px] backdrop-blur-[40px] flex items-center justify-center">
-          <div className="flex text-[14px] font-[400] leading-[18px] text-[#F1F1F1]">Доход каждые 4 часа:</div>
-          <div className="ml-[10px] mr-[20px] flex items-center gap-[5px] text-[14px] font-[600] leading-[18px] text-[#FFD900]">
-          {1000 * account?.character?.multiplier}
-          <img
-          className="flex w-[12px] h-[12px] mt-[-2px]"
-          src={raster3dIcon}
-          alt=""
-          />
+            <div className="flex text-[14px] font-[400] leading-[18px] text-[#F1F1F1]">Доход каждые 4 часа:</div>
+            <div className="ml-[10px] mr-[20px] flex items-center gap-[5px] text-[14px] font-[600] leading-[18px] text-[#FFD900]">
+              {1000 * account?.character?.multiplier}
+              <img
+                className="flex w-[12px] h-[12px] mt-[-2px]"
+                src={raster3dIcon}
+                alt=""
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              {new Date(account?.farming?.end_date) < new Date() ?
+              <div onClick={claimFarming} className="transform active:scale-[0.9] transition-transform cursor-pointer px-[12px] py-[6px] rounded-[5px] bg-[#FFD900] flex items-center justify-center text-[#494949] text-[12px] font-[600] leading-[12px]">Забрать</div>
+              :<div className="transform active:scale-[0.9] transition-transform cursor-pointer px-[12px] py-[6px] rounded-[5px] bg-[#494949] flex items-center justify-center text-[#292929] text-[12px] font-[600] leading-[12px]">Забрать</div>}
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-          {new Date(account?.farming?.end_date) < new Date() ?
-            <div onClick={claimFarming} className="transform active:scale-[0.9] transition-transform cursor-pointer px-[12px] py-[6px] rounded-[5px] bg-[#FFD900] flex items-center justify-center text-[#494949] text-[12px] font-[600] leading-[12px]">Забрать</div>
-            :<div className="transform active:scale-[0.9] transition-transform cursor-pointer px-[12px] py-[6px] rounded-[5px] bg-[#494949] flex items-center justify-center text-[#292929] text-[12px] font-[600] leading-[12px]">Забрать</div>}
-            </div>
-            </div>
-            </div>
-            <div className="absolute left-0 right-0 bottom-[65.33%] mx-auto z-[-1] w-[53.84%] h-[10%] bg-[#B331FF] rounded-[100%] blur-[50px]"></div>
-            <div className="absolute left-0 right-0 bottom-[29.06%] mx-auto z-[-1] w-[75.64%] h-[6.4%] bg-[#B331FF] rounded-[100%] blur-[50px]"></div>
-            <img
-            className="tron absolute z-[-1] w-[100%] scale-[1.582] bottom-[2%]"
-            src={tronImage}
-            alt=""
-            />
-            <img 
-            ref={imgRef}
-            className="pers cursor-pointer absolute z-[0] bottom-[21.06%] w-[100%] scale-[1.236] active:scale-[1.15] duration-[0.2s]"
-            src={personage}
-            alt=""
-            onClick={handleClick}
-            onTouchStart={handleTouch}
-            />
-            {plusOnes.map(plusOne => (
-              <div 
-              key={plusOne.id}
-              className="plusOne"
-              style={{ left: `${plusOne.x}px`, top: `${plusOne.y}px` }}
-              >
-              +{Number(account?.character?.multiplier)}
-              </div>
-            ))}
-            </div>
-            <Boost />
-            </>
-          );
-        }
-        
-        export default Home;
+        </div>
+        <div className="absolute left-0 right-0 bottom-[65.33%] mx-auto z-[-1] w-[53.84%] h-[10%] bg-[#B331FF] rounded-[100%] blur-[50px]"></div>
+        <div className="absolute left-0 right-0 bottom-[29.06%] mx-auto z-[-1] w-[75.64%] h-[6.4%] bg-[#B331FF] rounded-[100%] blur-[50px]"></div>
+        <img
+          className="tron absolute z-[-1] w-[100%] scale-[1.582] bottom-[2%]"
+          src={tronImage}
+          alt=""
+        />
+        <img 
+          ref={imgRef}
+          className="pers cursor-pointer absolute z-[0] bottom-[21.06%] w-[100%] scale-[1.236] active:scale-[1.15] duration-[0.2s]"
+          src={personage}
+          alt=""
+          onClick={handleClick}
+          onTouchStart={handleTouch}
+          onTouchEnd={() => {
+            if (navigator.vibrate) {
+              try {
+                navigator.vibrate(200); // Вибрируем 200 миллисекунд
+              } catch (e) {
+                alert(e);
+              }
+            } else {
+              alert(JSON.stringify(navigator))
+            }
+          }}
+        />
+        {plusOnes.map(plusOne => (
+          <div 
+            key={plusOne.id}
+            className="plusOne"
+            style={{ left: `${plusOne.x}px`, top: `${plusOne.y}px` }}
+          >
+            +{Number(account?.character?.multiplier)}
+          </div>
+        ))}
+      </div>
+      <Boost />
+    </>
+  );
+}
+
+export default Home;
