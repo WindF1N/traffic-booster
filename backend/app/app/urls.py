35c802935b56fef0ec42997d81e81090486a2c12
Object NSS <agafonov.egorushka@gmail.com>
2024-09-12 02:08:49 +0300

    add changes

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -9,17 +9,6 @@ from main.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponseForbidden
-import ipaddress
-
-def check_ip(request):
-    allowed_ips = ['10.0.0.0/24']  # Разрешенные IP-адреса из VPN-сети
-    client_ip = request.META.get('REMOTE_ADDR')
-
-    if not any(ipaddress.ip_address(client_ip) in ipaddress.ip_network(ip_range) for ip_range in allowed_ips):
-        return HttpResponseForbidden("Доступ запрещен")
-
-    return None
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,4 +27,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
-admin.site.check_ip = check_ip
