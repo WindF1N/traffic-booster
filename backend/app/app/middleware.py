4b771da18f386f12be3b34e2725515bf61b6ebf6
Object NSS <agafonov.egorushka@gmail.com>
2024-09-12 03:28:36 +0300

    add changes

--- backend/app/app/middleware.py
+++ backend/app/app/middleware.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseForbidden
 from django.urls import resolve
 from django.http import Http404
 
@@ -6,11 +7,12 @@ class WireGuardMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # Проверяем, является ли текущий путь частью админской панели
         if resolve(request.path_info).app_name == 'admin':
-            allowed_ips = ['94.25.182.30'] 
+            allowed_ips = ['94.25.182.30']  # Разрешенные IP-адреса из VPN-сети
             client_ip = request.META.get('HTTP_X_REAL_IP', request.META.get('REMOTE_ADDR', None))
             if not client_ip in allowed_ips:
-                raise Http404
+                return HttpResponseForbidden("Доступ запрещен для IP-адреса %s" % client_ip)
 
         response = self.get_response(request)
         return response
