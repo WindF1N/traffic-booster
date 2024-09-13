4f02ada67e6e745d433bf949b4d5ef73d983585d
Object NSS <agafonov.egorushka@gmail.com>
2024-09-14 02:18:10 +0300

    add changes

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -48,12 +48,7 @@ def create_invoice_link_sync(bot, character):
     except RuntimeError:  # no event loop running:
         loop = asyncio.new_event_loop()
     nest_asyncio.apply(loop)
-    asyncio.set_event_loop(loop)
-    try:
-        result = loop.run_until_complete(create_invoice_link_async(bot, character))
-    finally:
-        loop.close()
-    return result
+    return loop.run_until_complete(create_invoice_link_async(bot, character))
 
 class TelegramAuthView(APIView):
     def post(self, request):
