263eb608f5a737c354d4373d880e770acc10d627
Object NSS <agafonov.egorushka@gmail.com>
2024-09-14 02:22:40 +0300

    add changes

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -44,8 +44,10 @@ def asyncio_run(future, as_task=True):
         loop = asyncio.get_running_loop()
     except RuntimeError:  # no event loop running:
         loop = asyncio.new_event_loop()
+        nest_asyncio.apply(loop)
         return loop.run_until_complete(_to_task(future, as_task, loop))
     else:
+        nest_asyncio.apply(loop)
         return asyncio.run(_to_task(future, as_task, loop))
 
 
